ed8e6b780f1723d6c9404c4bdeb36b90daa74914
Batuhan Sahin <battusahin97@gmail.com>
2021-12-24 13:59:39 +0300

    pagination

--- blog/views/anasayfa.py
+++ blog/views/anasayfa.py
@@ -1,7 +1,14 @@
+from django.core import paginator
 from django.shortcuts import render
+from blog.models import YazilarModel
+from django.core.paginator import Paginator
 
 def anasayfa(request):
-    context = {
-        'isim' : 'Batuhan Şahin',
-    }
-    return render(request, 'pages/anasayfa.html', context=context)
+    
+    yazilar = YazilarModel.objects.order_by('-id')
+    sayfa = request.GET.get('sayfa')
+    paginator = Paginator(yazilar, 2)
+
+    return render(request, 'pages/anasayfa.html', context={
+        'yazilar':paginator.get_page(sayfa),
+    })
