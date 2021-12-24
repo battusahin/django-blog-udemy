ee544bc27a821de64a778813e36298d63d9880ba
Batuhan Sahin <battusahin97@gmail.com>
2021-12-24 15:20:43 +0300

    listeleme ve sayfalama tamamlandı.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from blog.views import iletisim, anasayfa
+from blog.views import iletisim, anasayfa, kategori
+from blog.views.yazilarim import yazilarim
 
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
     path('iletisim/', iletisim, name='iletisim'),
+    path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
+    path('yazilarim', yazilarim, name='yazilarim')
 ]
